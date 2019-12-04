0b489257d4cf7b2c8f4377c27ae0c8980e5c065b
Paul <34652585+pkrausee@users.noreply.github.com>
2019-12-04 14:00:39 +0100

    TODO

--- Zad 3/src/main/java/Main.java
+++ Zad 3/src/main/java/Main.java
@@ -2,6 +2,7 @@ import MatrixOperations.Recommend;
 
 public class Main {
     public static void main(String[] args) {
+       // TODO: Try setting the iterations number to 100.
         Recommend.calculate_ALS(0.1, 3, 10);
     }
 }
